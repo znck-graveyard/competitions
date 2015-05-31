1efb0a8b64be37093a10226200335a51f6747006
anjankaur <anjankaur01.mkr@gmail.com>
2015-06-01 02:14:11 +0530

    Contests and Constraints migrations with modifications

--- database/migrations/2015_05_31_104810_create_contests_table.php
+++ database/migrations/2015_05_31_104810_create_contests_table.php
@@ -3,50 +3,55 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateContestsTable extends Migration {
+class CreateContestsTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('contests',function(Blueprint $table)
-		{
-            $prize=[];
-            $submission_type=[];
-			$table->increments('id');
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contests', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('type');
             $table->string('name');
             $table->text('description');
-            $table->enum("submission_type",$submission_type);
+            $table->string("submission_type");
             $table->string('image');
-            $table->longText('rules');
+            $table->text('rules');
             $table->timestamp('start_date');
             $table->timestamp('end_date');
-            $table->enum("prize",$prize);
+            $table->string("prize");
             $table->boolean('peer_review_enabled');
             $table->decimal('peer_review_weightage');
-            $table->boolean('manual_review_enabled',5,2);
-            $table->decimal('manual_review_weightage',5,2);
-            $table->foreign('maintainer_id')->refrences('id')->on('users');
+            $table->boolean('manual_review_enabled');
+            $table->decimal('manual_review_weightage');
+            $table->integer('maintainer_id')->unsigned();
             $table->integer('max_entries');
-            $table->time('max_iteration');
+            $table->integer('max_iteration');
             $table->boolean('team_entry_enabled');
             $table->integer('team_size');
-			$table->timestamps();
-		});
-	}
+            $table->timestamps();
+            $table->foreign('maintainer_id')
+                ->refrences('id')
+                ->on('users');
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('contests');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contests', function ($table) {
+            $table->dropForeign('contests_maintainer_id_foreign');
+        });
+        Schema::drop('contests');
+
+    }
 
 }
--- database/migrations/2015_05_31_152506_create_constraints_table.php
+++ database/migrations/2015_05_31_152506_create_constraints_table.php
@@ -3,35 +3,39 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateConstraintsTable extends Migration {
+class CreateConstraintsTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('constraints', function(Blueprint $table)
-		{
-			$table->increments('id');
-            $table->foreign('contest_id')->refrences('id')->on('contests')->onDelete('cascade');
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('constraints', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('contest_id')->unsigned();
             $table->string('key');
             $table->string('condition');
             $table->string('value');
-            $table->boolean('optional');
-			$table->timestamps();
-		});
-	}
+            $table->boolean('optional')->default(0);
+            $table->timestamps();
+            $table->foreign('contest_id')
+                ->refrences('id')
+                ->on('contests')
+                ->onDelete('cascade');
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('constraints');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('constraints');
+    }
 
 }
