f2557dd0753e1f60f0f54d1ab0c6476cd67668af
Vaibhav Raj Gupta <gvaibhavraj2@gmail.com>
2015-06-03 17:50:24 +0530

    Moderator table update

--- database/migrations/2015_06_03_100339_create_moderators_table.php
+++ database/migrations/2015_06_03_100339_create_moderators_table.php
@@ -17,17 +17,13 @@ class CreateModeratorsTable extends Migration {
 			$table->increments('id');
 			$table->string('name');
 			$table->string('email');
+			$table->string('key');
+			$table->string('secret');
 			$table->integer('user_id')->unsigned()->index();
             $table->integer('contest_id')->unsigned()->index();
-            $table->timestamp('expiry')
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('users')
-                ->onUpdate('cascade');
-            $table->foreign('contest_id')
-                ->references('id')
-                ->on('contests')
-                ->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamp('expiry');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade');
+            $table->foreign('contest_id')->references('id')->on('contests')->onDelete('cascade')->onUpdate('cascade');
 			$table->timestamps();
 		});
 	}
@@ -39,6 +35,12 @@ class CreateModeratorsTable extends Migration {
 	 */
 	public function down()
 	{
+		Schema::table('moderators', function ($table) {
+            $table->dropIndex('moderators_contest_id_index');
+            $table->dropForeign('moderators_contest_id_foreign');
+            $table->dropIndex('moderators_user_id_index');
+            $table->dropForeign('moderators_user_id_foreign');
+        });
 		Schema::drop('moderators');
 	}
 
