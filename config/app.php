77540ce1efc08f89c6bc26509034b45b459d7fcc
Vaibhav Raj Gupta <gvaibhavraj2@gmail.com>
2015-06-08 19:19:15 +0530

    basic create function

--- config/app.php
+++ config/app.php
@@ -135,7 +135,7 @@ return [
 		'Illuminate\Translation\TranslationServiceProvider',
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
-
+		'Znck\Flash\FlashServiceProvider', #Flash notifications
 		/*
 		 * Application Service Providers...
 		 */
@@ -192,6 +192,7 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+		'Flash' => 'Znck\Flash\Flash',
 
 	],
 
