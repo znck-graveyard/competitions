04d2d1de71ab1276ac6c7cdf0b551e0b001469d6
anjankaur <anjankaur01.mkr@gmail.com>
2015-06-13 16:44:38 +0530

    Contest controller

--- app/Http/Controllers/ContestController.php
+++ app/Http/Controllers/ContestController.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class ContestController extends Controller
 {
@@ -20,12 +22,13 @@ class ContestController extends Controller
     function __construct(Guard $auth)
     {
         $this->user = $auth->user();
-        $this->middleware('auth', ['except' => ['contestCategoryHome', 'show']]);
+        $this->middleware('auth', ['only' => ['create', 'update', 'edit']]);
     }
 
     /**
      * Display a listing of the resource.
      *
+     * @param $type
      * @return Response
      */
     public function contestCategoryHome($type)
@@ -59,7 +62,7 @@ class ContestController extends Controller
      */
     public function storeFirstTime(Requests\UserDetailsRequest $request)
     {
-        
+
         $id = $this->user->id();
         $moderator = User::find($id);
         $moderator->first_name = ucfirst($request->get('first_name'));
@@ -67,15 +70,83 @@ class ContestController extends Controller
         $moderator->email = $request->get('email');
         $moderator->date_of_birth = $request->get('date_of_birth');
         $moderator->gender = $request->get('gender');
+
+        $this->userAttributes($request, $id);
+        return view('contest.create');
+    }
+
+
+    /**
+     * Entering User Attributes
+     * @param Requests\UserDetailsRequest $request
+     * @param $id
+     */
+    public function userAttributes(Requests\UserDetailsRequest $request, $id)
+    {
+        $user_attribute = [];
         $short_bio = $request->get('short_bio');
-        $user_attribute = [
+        $user_attribute_short_bio = [
             'user_id' => $id,
             'key' => 'short_bio',
             'value' => $short_bio
         ];
-        UserAttribute::create($user_attribute);
+        $user_attributes[] = $user_attribute_short_bio;
 
-        return view('contest.create');
+        if ($request->hasFile('profile_pic')) {
+            $file = $request->file('profile_pic');
+            $extension = $file->getClientOriginalExtension();
+            $filename = $file->getFilename() . '.' . $extension;
+            Storage::disk('local')->put($filename, File::get($file));
+            $user_attribute_profile_pic = [
+                'user_id' => $id,
+                'key' => 'profile_pic',
+                'value' => $filename
+            ];
+            $user_attributes[] = $user_attribute_profile_pic;
+        }
+        if ($request->hasFile('cover_image')) {
+            $file = $request->file('cover_image');
+            $extension = $file->getClientOriginalExtension();
+            $filename = $file->getFilename() . '.' . $extension;
+            Storage::disk('local')->put($filename, File::get($file));
+            $user_attribute_cover_image = [
+                'user_id' => $id,
+                'key' => 'cover_image',
+                'value' => $filename
+            ];
+            $user_attributes[] = $user_attribute_profile_pic;
+        }
+
+        if ($request->has('facebook_username')) {
+            $user_attribute_facebook = [
+                'user_id' => $id,
+                'key' => 'facebook_username',
+                'value' => $request->get('facebook_username')
+            ];
+            $user_attributes[] = $user_attribute_facebook;
+
+        }
+        if ($request->has('twitter_username')) {
+            $user_attribute_twitter = [
+                'user_id' => $id,
+                'key' => 'twitter_username',
+                'value' => $request->get('twitter_username')
+            ];
+            $user_attributes[] = $user_attribute_twitter;
+        }
+        if ($request->has('instagram_username')) {
+            $user_attribute_instagram = [
+                'user_id' => $id,
+                'key' => 'instagram_username',
+                'value' => $request->get('instagram_username')
+            ];
+            $user_attributes[] = $user_attribute_instagram;
+
+        }
+        foreach ($user_attributes as $attribute) {
+            UserAttribute::create($attribute);
+        }
+        return;
     }
 
     /**
@@ -83,11 +154,123 @@ class ContestController extends Controller
      *
      * @param Requests\CreateContestRequest $request
      * @return Response
+     * @throws \Exception
      */
 
     public function store(Requests\CreateContestRequest $request)
     {
-        $contest = new Contest();
+        \DB::beginTransaction();
+        try {
+            $contest = $this->createOrUpdateContest($request);
+
+            $this->user->is_maintainer = true;
+
+            flash()->success("New Contest created successfully!!");
+
+        } catch (\Exception $e) {
+            \DB::rollBack();
+            throw $e;
+        }
+
+        \DB::commit();
+        return redirect('contest/{type}');//not sure about return
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $contest = Contest::findOrFail($id);
+        return view('contest.show', compact('contest'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $contest = Contest::find($id);
+        $user_id = $this->user->id();
+        $maintainer_id = $contest->maintainer_id;
+        if ($user_id == $maintainer_id) {
+            return view('contest.create', compact('contest'));
+        } else {
+            flash()->warning("Access Denied");
+            return redirect()->back();
+
+        }
+
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @param Requests\CreateContestRequest $request
+     * @return Response
+     * @throws \Exception
+     */
+    public function update($id, Requests\CreateContestRequest $request)
+    {
+        $contest = Contest::find($id);
+        $user_id = $this->user->id();
+        $maintainer_id = $contest->maintainer_id;
+        if ($user_id == $maintainer_id) {
+            \DB::beginTransaction();
+            try {
+                $this->createOrUpdateContest($request, $contest);
+            } catch (\Exception $e) {
+                \DB::rollBack();
+                throw $e;
+            }
+
+            \DB::commit();
+        } else {
+            flash()->warning("Access Denied");
+            return redirect()->back();
+
+        }
+
+        return redirect('contest/{type}');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        $contest = Contest::find($id);
+        $user_id = $this->user->id();
+        $maintainer_id = $contest->maintainer_id;
+        if ($user_id == $maintainer_id) {
+            $contest = Contest::find($id);
+            $contest->delete();
+            return redirect()->home();
+        } else {
+            flash()->warning("Access Denied");
+            return redirect()->back();
+        }
+
+    }
+
+    public function createOrUpdateContest(Requests\CreateContestRequest $request, $contest = null)
+    {
+        if (is_null($contest)) {
+            $contest = new Contest();
+        }
+
         $s_date = $request->get('start_contest_date');//start date from form
         $s_time = $request->get('start_contest_time');//start time from form
         $s_timestamp = \Carbon::createFromTimestamp($s_date . $s_time);
@@ -118,56 +301,31 @@ class ContestController extends Controller
 
         $contest->team_entry_enabled = $request->get('team_entry_enabled');
         $contest->maintainer_id = $this->user->id();
-
+        if ($request->hasFile('contest_banner')) {
+            $file = $request->file('contest_banner');
+            $extension = $file->getClientOriginalExtension();
+            $filename = $file->getFilename() . '.' . $extension;
+            Storage::disk('local')->put($filename, File::get($file));
+            $contest->image = $filename;
+        }
         $contest->save();
-        $this->user->is_maintainer = true;
 
-        return redirect('contest/{type}');//not sure about return
+        foreach ($request->judges as $judge) {
 
-    }
+            DB::table('judges')->insert(
+                array(
+                    'contest_id' => $contest->id,
+                    'user_id' => $judge['user_id'],
+                    'link' => $judge['link'],
+                    'email' => $judge['email']
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        $contest = Contest::findOrFail($id);
-        return view('contest.show', compact('contest'));
-    }
+                )
+            );
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function update($id)
-    {
-        //
-    }
+        return $contest;
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
 
     }
 
