b32ffaeac6c4452c6181ba0f14735ff0513efc5a
Vaibhav Raj Gupta <gvaibhavraj2@gmail.com>
2015-06-15 20:47:16 +0530

    Entry request update

--- app/Http/Requests/CreateEntryRequest.php
+++ app/Http/Requests/CreateEntryRequest.php
@@ -1,7 +1,7 @@
-+<?php namespace App\Http\Requests;
-+
-+use App\Http\Requests\Request;
-+
+<?php namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
 class CreateEntryRequest extends Request
 {
 
