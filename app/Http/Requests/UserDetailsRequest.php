5f4297302e72ec5b829cb3355e5e8f0abb2583c1
Vaibhav Raj Gupta <gvaibhavraj2@gmail.com>
2015-06-15 20:49:12 +0530

    user details request for first time submission

--- app/Http/Requests/UserDetailsRequest.php
+++ app/Http/Requests/UserDetailsRequest.php
@@ -2,6 +2,7 @@
 
 use App\Http\Requests\Request;
 
+
 class UserDetailsRequest extends Request
 {
 
@@ -47,3 +48,37 @@ class UserDetailsRequest extends Request
     }
 
 }
+
+class UserDetailsRequest extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+        //TODO
+		return false;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'first_name'=>'required',
+            'last_name'=>'required',
+            'username' => 'required|string|unique',
+            'email'=>'required|email|unique',
+            'date_of_birth'=>'required|date',
+            'gender'=>'required',
+            'short_bio'=>'required'
+		];
+	}
+
+}
+
