3cd534aff2fd3b1730fd9d15085f86d8203621da
anjankaur <anjankaur01.mkr@gmail.com>
2015-06-27 22:55:41 +0530

    entry

---
+++ resources/views/entries/entry_submit.blade.php
@@ -0,0 +1,18 @@
+@extends('app')
+@section('content')
+
+    <div class="jumbotron picCheer darken">
+        <hgroup style="text-align: center">
+            <h1>Participate in the Contest</h1>
+
+            <h3>Create opportunities, discover amazing talents, stay inspired. </h3>
+        </hgroup>
+
+    </div>
+
+    <div class="container">
+        <p>Please upload your entry in required file format and a small abstract to support the same. </p>
+
+    </div>
+
+@endsection
