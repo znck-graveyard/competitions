aa3ee376611d225413d4834e24750492dd5ca4c3
Rahul Kadyan <hi@znck.me>
2015-06-30 19:47:23 +0530

    update compiled path variable

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -23,8 +23,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 | To dramatically increase your application's performance, you may use a
 | compiled class file which contains all of the classes commonly used
-| by a request. The Artisan "optimize" is used to create this file.
-|
+| by a request. The Artisan "optimize" is used to create this fil
 */
 
 $compiledPath = __DIR__.'/cache/compiled.php';
