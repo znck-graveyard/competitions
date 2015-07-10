ca9a07e6904587eeeee26c47ef1ce17fef1e65fd
Rahul Kadyan <hi@znck.me>
2015-07-10 21:10:27 +0530

    fix logout error

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -26,7 +26,7 @@
                         <ul class="dropdown-menu" role="menu">
                             <li><a href="{{ route('me') }}">Profile</a></li>
                             <li><a href="{{ route('me.preferences') }}">Preferences</a></li>
-                            <li><a href="{{ route('auth.login') }}">Logout</a></li>
+                            <li><a href="{{ route('auth.logout') }}">Logout</a></li>
                         </ul>
                     </li>
                 @endif
