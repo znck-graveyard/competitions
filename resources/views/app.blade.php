6776a812c2a48693653721c1c5f16eca256ff05f
Rahul Kadyan <hi@znck.me>
2015-07-10 23:29:46 +0530

    add description meta tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Whizzspace is an online talent platform where people can showcase their skills and compete to win fame and fortune."/>
 
     <title>@yield('title',  'Whizzspace')</title>
 
