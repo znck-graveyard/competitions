cc6380808bcb509ecb8e92dd97237f23800f9ec9
anjankaur <anjankaur01.mkr@gmail.com>
2015-07-18 12:53:11 +0530

    Minor change dob

--- resources/views/profile/create.blade.php
+++ resources/views/profile/create.blade.php
@@ -10,12 +10,17 @@
 
     <script>
         $(document).ready(function () {
+
             $('.input-group.date').datepicker({
                 autoclose: true,
                 todayHighlight: true,
                 format: 'yyyy-mm-dd'
-            }).datepicker('update', new Date());
-
+            })
+            @if($user->date_of_birth)
+                .datepicker('update', "{{ $user->date_of_birth->format('Y-m-d') }}");
+            @else
+                .datepicker('update', new Date);
+            @endif
         });
     </script>
 @endsection
@@ -42,25 +47,29 @@
                 <div class="col-xs-12 col-sm-6">
                     <div class="form-group">
                         <label for="first-name" class="text-uppercase required">First Name</label>
-                        {!! Form::text('first_name', null, ['class' => 'input-lg form-control', 'required' =>'', 'placeholder' => 'enter your first name', 'id' => 'first-name']) !!}
+                        {!! Form::text('first_name', null, ['class' => 'input-lg form-control', 'required' =>'',
+                        'placeholder' => 'enter your first name', 'id' => 'first-name']) !!}
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-6">
                     <div class="form-group">
                         <label for="last-name" class="text-uppercase required">Last Name</label>
-                        {!! Form::text('last_name', null, ['class' => 'input-lg form-control', 'required' =>'', 'placeholder' => 'enter your last name', 'id' => 'last-name']) !!}
+                        {!! Form::text('last_name', null, ['class' => 'input-lg form-control', 'required' =>'',
+                        'placeholder' => 'enter your last name', 'id' => 'last-name']) !!}
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-6">
                     <div class="form-group">
                         <label for="username" class="text-uppercase required">Username</label>
-                        {!! Form::text('username', null, ['class' => 'input-lg form-control', 'required' =>'', 'placeholder' => 'choose your username', 'id' => 'username']) !!}
+                        {!! Form::text('username', null, ['class' => 'input-lg form-control', 'required' =>'',
+                        'placeholder' => 'choose your username', 'id' => 'username']) !!}
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-6">
                     <div class="form-group">
                         <label for="email" class="text-uppercase required">email</label>
-                        {!! Form::email('email', null, ['class' => 'input-lg form-control', 'required' =>'', 'placeholder' => 'enter your email', 'id' => 'email']) !!}
+                        {!! Form::email('email', null, ['class' => 'input-lg form-control', 'required' =>'',
+                        'placeholder' => 'enter your email', 'id' => 'email']) !!}
                     </div>
                 </div>
 
@@ -69,7 +78,9 @@
                         <label class="text-uppercase required" for="dob">Date of Birth</label>
 
                         <div class="input-group date">
-                            {!! Form::text('date_of_birth', $user->date_of_birth ? $user->date_of_birth->format('Y-m-d') : old('date_of_birth'), ['class' => 'input-lg form-control', 'required' =>'', 'placeholder' => 'select your date of birth', 'id' => 'dob', 'data-type'=>'date']) !!}
+                            {!! Form::text('date_of_birth', $user->date_of_birth ? $user->date_of_birth->format('Y-m-d')
+                            : old('date_of_birth'), ['class' => 'input-lg form-control', 'required' =>'', 'placeholder'
+                            => 'select your date of birth', 'id' => 'dob', 'data-type'=>'date']) !!}
                             <span class="input-group-addon" style="cursor: pointer">
                                 <i class="glyphicon glyphicon-calendar"></i>
                             </span>
@@ -80,7 +91,9 @@
                 <div class="col-xs-12 col-sm-6">
                     <div class="form-group">
                         <label class="text-uppercase required" for="gender">Gender</label>
-                        {!! Form::select('gender', ['male' => 'male', 'female' => 'female', 'other' => 'other'], null, ['class' => 'input-lg form-control', 'required' =>'', 'placeholder' => 'enter your first name', 'id' => 'gender']) !!}
+                        {!! Form::select('gender', ['male' => 'male', 'female' => 'female', 'other' => 'other'], null,
+                        ['class' => 'input-lg form-control', 'required' =>'', 'placeholder' => 'enter your first name',
+                        'id' => 'gender']) !!}
                     </div>
                 </div>
 
@@ -100,7 +113,8 @@
                                 </div>
 
                                 <div>
-                                    {!! Form::text('profile_photo_link', old('cover_link'), ['class' => 'input-lg form-control', 'placeholder' => 'enter link of the photo',]) !!}
+                                    {!! Form::text('profile_photo_link', old('cover_link'), ['class' => 'input-lg
+                                    form-control', 'placeholder' => 'enter link of the photo',]) !!}
                                 </div>
                             </div>
                         </div>
@@ -128,7 +142,8 @@
                                 </div>
 
                                 <div>
-                                    {!! Form::text('cover_photo_link', old('cover_link'), ['class' => 'input-lg form-control', 'placeholder' => 'enter link of the photo',]) !!}
+                                    {!! Form::text('cover_photo_link', old('cover_link'), ['class' => 'input-lg
+                                    form-control', 'placeholder' => 'enter link of the photo',]) !!}
                                 </div>
                             </div>
                         </div>
@@ -143,7 +158,8 @@
                 <div class="col-xs-12 col-sm-6">
                     <div class="form-group">
                         <label class="text-uppercase required" for="bio">A short bio</label>
-                        {!! Form::textarea('bio', old('bio'), ['class' => 'input-lg form-control', 'required' =>'', 'placeholder' => 'tell everyone about you', 'id' => 'bio']) !!}
+                        {!! Form::textarea('bio', old('bio'), ['class' => 'input-lg form-control', 'required' =>'',
+                        'placeholder' => 'tell everyone about you', 'id' => 'bio']) !!}
                     </div>
                 </div>
 
@@ -153,21 +169,24 @@
                     <div class="form-group">
                         <div class="input-with-icon">
                             <label for="twitter"><img src="{{ asset('image/social/twitter.png') }}"/></label>
-                            {!! Form::text('connection_twitter', null, ['class' => 'input-lg form-control', 'placeholder' => 'twitter.com/username', 'id' => 'twitter']) !!}
+                            {!! Form::text('connection_twitter', null, ['class' => 'input-lg form-control',
+                            'placeholder' => 'twitter.com/username', 'id' => 'twitter']) !!}
                         </div>
                     </div>
 
                     <div class="form-group">
                         <div class="input-with-icon">
                             <label for="facebook"><img src="{{ asset('image/social/facebook.png') }}"/></label>
-                            {!! Form::text('connection_facebook', null, ['class' => 'input-lg form-control', 'placeholder' => 'facebook.com/username', 'id' => 'facebook']) !!}
+                            {!! Form::text('connection_facebook', null, ['class' => 'input-lg form-control',
+                            'placeholder' => 'facebook.com/username', 'id' => 'facebook']) !!}
                         </div>
                     </div>
 
                     <div class="form-group">
                         <div class="input-with-icon">
                             <label for="instagram"><img src="{{ asset('image/social/instagram.png') }}"/></label>
-                            {!! Form::text('connection_instagram', null, ['class' => 'input-lg form-control', 'placeholder' => 'instagram.com/username', 'id' => 'instagram']) !!}
+                            {!! Form::text('connection_instagram', null, ['class' => 'input-lg form-control',
+                            'placeholder' => 'instagram.com/username', 'id' => 'instagram']) !!}
                         </div>
                     </div>
                 </div>
